915a583478591ba61cc10999a11cc249c298ccad
Mark Ito <markito3@gmail.com>
2022-07-27 19:20:34 -0400

    improvements

--- vorbistag.py
+++ vorbistag.py
@@ -3,9 +3,13 @@
 import glob
 import os
 
-artist = "Carole King"
-album = "Tapestry"
 file = open("tracks.txt", 'r')
+artist = file.readline()
+album = file.readline()
+separator = file.readline()
+if separator != "---\n":
+    print("error: separator not found")
+    exit(1)
 tracknumber = 0
 while True:
     title = file.readline()
@@ -13,8 +17,8 @@ while True:
         break
     tracknumber += 1
     tags = open("tags.tmp", "w")
-    tags.write("ARTIST=" + artist + "\n")
-    tags.write("ALBUM=" + album + "\n")
+    tags.write("ARTIST=" + artist)
+    tags.write("ALBUM=" + album)
     tags.write("TRACKNUMBER=" + str(tracknumber) + "\n")
     tags.write("TITLE=" + title.strip() + "\n")
     tags.close()
@@ -23,14 +27,14 @@ while True:
     oggfile_in = oggfiles[0].replace(" ", "\ ")
     #print(oggfiles)
     title_escaped = title
-    print(title_escaped)
     title_escaped = title.replace(" ", "_")
-    print(title_escaped)
     title_escaped = title_escaped.replace("'", "_")
-    print(title_escaped)
     title_escaped = title_escaped.replace("&", "and")
     title_escaped = title_escaped.replace("(", "\(")
     title_escaped = title_escaped.replace(")", "\)")
+    title_escaped = title_escaped.replace(";", ",")
+    title_escaped = title_escaped.replace("?", ".")
+    print(title_escaped)
     oggfile_out = tracknumber_str + "-" + title_escaped
     vc_command = "vorbiscomment -w -c tags.tmp " + oggfile_in + " " + oggfile_out.strip() + ".ogg"
     print(vc_command)
